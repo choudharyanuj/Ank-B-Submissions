b5a82bd4418ed94721afb57d2ebab2c5b8cb878b
choudhary-ankit <ankit_chy@outlook.com>
2020-07-14 18:52:41 +0530

    W8 D1 C1

---
+++ week_8/day_1/coding_1/github/src/data.js
@@ -0,0 +1,92 @@
+
+    const HotelInformation = [
+        {
+            title: "Mutan Kabab",
+            address: "Anishabad, Patna",
+            price: "min $3 up to 30 min",
+            Payment: "Accept online Payment only",
+            start: 3.5,
+            votes: "766 Votes",
+            Reviews: "512 reviews",
+            mode: "order Online",
+            image: "http://www.tasty-indian-recipes.com/wp-content/uploads/2012/09/Mutton-Seekh-Kebab.jpg"    
+        },
+        {
+            title: "Foodaholic",
+            address: "Boring-Road, Patna",
+            price: "min $5 up to 30 min",
+            Payment: "Accept online Payment only",
+            start: 4.5,
+            votes: "1006 Votes",
+            Reviews: "52 reviews",
+            mode: "order Online",
+            image: "https://www.inspiredtaste.net/wp-content/uploads/2016/09/Homemade-Creamy-Vegtable-Soup-Recipe-1-1200.jpg"    
+        },
+        {
+            title: "Foodaholic",
+            address: "Boring-Road, Patna",
+            price: "min $5 up to 30 min",
+            Payment: "Accept online Payment only",
+            start: 4.5,
+            votes: "1006 Votes",
+            Reviews: "52 reviews",
+            mode: "order Online",
+            image: "https://www.inspiredtaste.net/wp-content/uploads/2016/09/Homemade-Creamy-Vegtable-Soup-Recipe-1-1200.jpg"    
+        },
+        {
+            title: "Foodaholic",
+            address: "Boring-Road, Patna",
+            price: "min $5 up to 30 min",
+            Payment: "Accept online Payment only",
+            start: 2.5,
+            votes: "1006 Votes",
+            Reviews: "52 reviews",
+            mode: "order Online",
+            image: "https://www.inspiredtaste.net/wp-content/uploads/2016/09/Homemade-Creamy-Vegtable-Soup-Recipe-1-1200.jpg"    
+        },
+        {
+            title: "Foodaholic",
+            address: "Boring-Road, Patna",
+            price: "min $5 up to 30 min",
+            Payment: "Accept online Payment only",
+            start: 1.5,
+            votes: "1006 Votes",
+            Reviews: "52 reviews",
+            mode: "order Online",
+            image: "https://www.inspiredtaste.net/wp-content/uploads/2016/09/Homemade-Creamy-Vegtable-Soup-Recipe-1-1200.jpg"    
+        },
+        {
+            title: "Foodaholic",
+            address: "Boring-Road, Patna",
+            price: "min $5 up to 30 min",
+            Payment: "Accept online Payment only",
+            start: 4.5,
+            votes: "1006 Votes",
+            Reviews: "52 reviews",
+            mode: "order Online",
+            image: "https://www.inspiredtaste.net/wp-content/uploads/2016/09/Homemade-Creamy-Vegtable-Soup-Recipe-1-1200.jpg"    
+        },
+        {
+            title: "Foodaholic",
+            address: "Boring-Road, Patna",
+            price: "min $5 up to 30 min",
+            Payment: "Accept online Payment only",
+            start: 3.0,
+            votes: "1006 Votes",
+            Reviews: "52 reviews",
+            mode: "order Online",
+            image: "https://www.inspiredtaste.net/wp-content/uploads/2016/09/Homemade-Creamy-Vegtable-Soup-Recipe-1-1200.jpg"    
+        },
+        {
+            title: "Foodaholic",
+            address: "Boring-Road, Patna",
+            price: "min $5 up to 30 min",
+            Payment: "Accept online Payment only",
+            start: 2.0,
+            votes: "1006 Votes",
+            Reviews: "52 reviews",
+            mode: "order Online",
+            image: "https://www.inspiredtaste.net/wp-content/uploads/2016/09/Homemade-Creamy-Vegtable-Soup-Recipe-1-1200.jpg"    
+        }
+    ]
+export default HotelInformation;
