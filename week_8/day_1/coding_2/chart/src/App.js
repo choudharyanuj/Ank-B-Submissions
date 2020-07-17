c411706eaddca6cbf927da097430dd4de24724ba
choudhary-ankit <ankit_chy@outlook.com>
2020-07-17 09:42:41 +0530

    chart

---
+++ week_8/day_1/coding_2/chart/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import Demo from './Demo'
+
+// const styles = {
+//   fontFamily: 'sans-serif',
+//   textAlign: 'center',
+// };
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+          <div>
+              <Demo/>
+          </div>
+      </div>
+    )
+  }
+}
+
+
+
+
